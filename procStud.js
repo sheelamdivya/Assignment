881f0ec0d82f165ec1076f55ff31cce386b68f14
sheelamdivya <sheelamdivya82@gmail.com>
2025-11-25 23:19:20 +0530

    Added procStud

---
+++ procStud.js
@@ -0,0 +1,25 @@
+function processStudents(students){
+    const filtered = students.filter(student => student.marks>60)
+    const sorted = filtered.sort((a,b) => b.marks - a.marks)
+    const names = sorted.map(student => student.name);
+
+    return names;
+}
+const students = [
+    {name : "Alice", marks : 58},
+    {name : "Bob", marks : 85},
+    {name : "Charlie", marks : 92},
+    {name : "David", marks : 45},
+    {name : "Emma", marks : 76},
+    {name : "Frank", marks : 63},
+    {name : "Grace", marks : 89},
+    {name : "Hannah", marks : 95},
+    {name : "Ian", marks : 54},
+    {name : "Kate", marks : 67},
+    {name : "Leo", marks : 88},
+    {name : "Ria", marks : 91},
+    {name : "Jack", marks : 79},
+    {name : "Nathan", marks : 72},
+    {name : "Olivia", marks : 82}
+]
+console.log(processStudents(students))
